11b0c2062a34105cdde9801330357beb84acb147
Patrick Hoogkamer <p.hoogkamer@intermax.nl>
2020-08-18 16:20:10 +0200

    feat: Implement docsJson endpoint

--- src/Docs/DocsController.php
+++ src/Docs/DocsController.php
@@ -2,16 +2,26 @@
 
 namespace Intermax\LaravelOpenApi\Docs;
 
+use cebe\openapi\exceptions\TypeErrorException;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
+use Intermax\LaravelOpenApi\Generator\Generator;
+use Throwable;
 
 class DocsController
 {
-    public function docsJson(ResponseFactory $responses): JsonResponse
+    /**
+     * @param Generator $generator
+     * @param ResponseFactory $responses
+     * @return JsonResponse
+     * @throws Throwable
+     * @throws TypeErrorException
+     */
+    public function docsJson(Generator $generator, ResponseFactory $responses): JsonResponse
     {
-        return $responses->json();
+        return $responses->json(json_decode($generator->generate()), 200, [], JSON_PRETTY_PRINT);
     }
 
     public function docs(Factory $views): View
