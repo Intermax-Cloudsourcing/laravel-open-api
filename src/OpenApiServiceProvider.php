6c6d9b95629fa722d50284cf21892adfd34403a0
Patrick Hoogkamer <p.hoogkamer@intermax.nl>
2020-08-18 16:14:19 +0200

    fix: web and config paths

--- src/OpenApiServiceProvider.php
+++ src/OpenApiServiceProvider.php
@@ -10,7 +10,7 @@ class OpenApiServiceProvider extends ServiceProvider
     {
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'open-api');
 
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
 
         $this->publishes([
             __DIR__ . '/../config/open-api.php' => config_path('open-api.php')
@@ -19,6 +19,6 @@ class OpenApiServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/open-api', 'open-api');
+        $this->mergeConfigFrom(__DIR__ . '/../config/open-api.php', 'open-api');
     }
 }
